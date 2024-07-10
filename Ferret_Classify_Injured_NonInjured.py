c2085e4a2141df09fbcb5edf114db0a35ee6d9d7
Neva <nevao@uw.edu>
2024-07-10 10:39:05 -0700

    Removed all shuffling, just perform simple logistic regression for binary target (total gross score)

--- Ferret_Classify_Injured_NonInjured.py
+++ Ferret_Classify_Injured_NonInjured.py
@@ -11,13 +11,14 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from helper_functions import remove_columns_with_substring, write_list_to_file, plot_feature_distributions
 import numpy as np
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, train_test_split
 from sklearn.compose import ColumnTransformer
 from ShuffleSeries import shuffle_series
 from Run_log_reg_on_ttsplit_with_treat import run_log_reg_on_ttsplit
+import sys
 
 # Choose target variable
-target = 'total gross score' #options: 'combined injury score', 'Pathology Score', 'total gross score', 'avg_5.30', 'Overall Sulci Sum', 'Overall Gyri Sum'
+target = 'total gross score' #options: 'Pathology Score', 'total gross score', 'avg_5.30', 'Overall Sulci Sum', 'Overall Gyri Sum'
 # Choose group
 group_to_test = 'Control and Vehicle'
 outputdir = '/home/toddr/neva/PycharmProjects/WoodAnimalData'
@@ -54,8 +55,6 @@ print("Number of rows containing the string '-':", count)
 #replace '-' with nan
 ferret.replace('-', np.nan, inplace=True)
 
-
-
 # ## Count and print total number of null values for each column with nans
 nan_cols = ferret.isna().sum()
 columns_with_nans = nan_cols[nan_cols > 0]
@@ -65,15 +64,16 @@ print(columns_with_nans)
 ferret.dropna(inplace=True)
 ferret.reset_index(inplace=True, drop=True)
 
-# Confirm there are no nan values left in dataframe
-nan_vals = ferret.isna().sum().sum()
-
-#recode gender as binary feature M=1, F=0
+# Recode gender as numeric categorical feature M=1, F=0
 ferret['Sex'] = ferret['Sex'].replace('F', 0) #female = 0
 ferret['Sex'] = ferret['Sex'].replace('M', 1) #male = 1
 
-#make total gross score a binary columns because there are so few cases with values >1
-# ferret.loc[ferret['total gross score'] > 1, 'total gross score'] = 1
+# Give target variables simpler names
+ferret.rename(columns={'Cortical Lesion (0-4) + Mineralization (0-4)': 'total gross score',
+               'Morph. Injury Score (path+BM+WM)': 'Pathology Score'}, inplace=True)
+
+# Make total gross score a binary columns because there are so few cases with values >1
+ferret.loc[ferret['total gross score'] > 1, 'total gross score'] = 1
 
 # give every subject group a number
 controlcode = 0  # Control = 0
@@ -81,6 +81,7 @@ vehiclecode = 1  # Vehicle = 1
 epocode = 2  # Epo = 2
 thcode = 3  # TH = 3
 
+# Code group as number instead of string
 ferret['Group'] = ferret['Group'].replace(['Control'], controlcode)
 ferret['Group'] = ferret['Group'].replace(['Veh'], vehiclecode)
 ferret['Group'] = ferret['Group'].replace(['Vehicle'], vehiclecode)
@@ -93,33 +94,47 @@ for col in ferret.columns:
         ferret[col] = pd.to_numeric(ferret[col])
 
 #Plot histograms of features
-plot_feature_distributions(ferret, ferret.columns)
+# plot_feature_distributions(ferret, ferret.columns)
 
-#calculate correlation matrix
-print(ferret.dtypes)
+# Check column datatypes
+dt = ferret.dtypes
 
-# Select Group Index as
-group_index = epocode
-
-group_of_interest_indices = ferret.index[ferret['Trx'] < group_index].tolist()
-
-ferret = ferret.iloc[group_of_interest_indices, :]
-ferret.reset_index(inplace=True, drop=True)
+# Remove all cases where group is not control or vehicle
+ferret_cv = ferret[ferret['Group'].isin([1,2])]
+ferret_cv.reset_index(inplace=True, drop=True)
 
 #select feature columns
-all_columns=ferret.columns.tolist()
-subj_info_columns = ['ID', 'Trx']
+all_columns=ferret_cv.columns.tolist()
+subj_info_columns = ['ID', 'Group']
 
-response_columns = ['Pathology Score', 'total gross score', 'avg_5.30','Overall Sulci Sum', 'Overall Gyri Sum']
+response_columns = ['Pathology Score', 'total gross score']
 feature_columns = [x for x in all_columns if ((x not in subj_info_columns) and (x not in response_columns))]
 
-# Split the data into 80/20 train-test split stratified by the target column in the X dataframe
-stratified_split = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+# Split the data into 80/20 train-test split stratified by the target column
 
-nshuffles=1000
-unique_shuffled_series = shuffle_series(ferret[target], nshuffles)
+split_df = ferret_cv.groupby('ID').mean()
+split_df = split_df.iloc[:,0:4]
+split_df.reset_index(inplace=True)
 
-ferret_orig=ferret.copy()
+# Check that the groupby mean operation did not change the values for each subject for the variables with only two possible values
+if not split_df['Group'].isin([1.0, 2.0]).all():
+    print(f'Error: Values in "Group" column are not limited to 1 and 2. Exiting program')
+    sys.exit(1)
+if not split_df['Sex'].isin([0.0, 1.0]).all():
+    print(f'Error: Values in "Sex" column are not limited to 0 and 1. Exiting program')
+    sys.exit(1)
+if not split_df['total gross score'].isin([0.0, 1.0]).all():
+    print(f'Error: Values in "total gross score" column are not limited to 0 and 1. Exiting program')
+    sys.exit(1)
+
+X = split_df[['ID', 'Sex']]
+y = pd.DataFrame(split_df[target], columns=[target])
+
+stratify_by = pd.concat([X['Sex'], y[target]], axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=stratify_by, test_size=0.2, random_state=42)
+Xtrain = ferret_cv[ferret_cv['ID'].isin(X_train['ID'])]
+test = ferret_cv[ferret_cv['ID'].isin(X_test['ID'])]
 
 # create column transformer for standard scalar
 # choose all columns except for the first column (sex)
@@ -127,34 +142,13 @@ columns_to_scale = list(range(1, len(feature_columns)))
 transformer = ColumnTransformer(transformers=[('scale', StandardScaler(), columns_to_scale)],
                                 remainder='passthrough')
 
-# Create the pipeline for logistic regression (use for Pathology Score and total gross score which are made binary above)
+# Create the pipeline for logistic regression (use for total gross score which was made binary)
 pipe_logreg = Pipeline([
     ('t', transformer),
-    ('pca', PCA(n_components=21)),
+    ('pca', PCA()),
     ('logreg', LogisticRegression(solver='liblinear', penalty='l1', C=0.04, random_state=42))
 ])
 
-auctrain_list=[]
-auctest_list=[]
-for rep in range(nshuffles):
-    print(f'REP {rep}')
-
-    shuff = pd.Series(unique_shuffled_series[rep])
-    ferret[target] = shuff
-
-    #Make a dataframe with just features and a target dataframe with just the response variable
-    X = ferret[feature_columns].copy()
-    y = ferret[[target]].copy()
-
-    final_avg_auc_roc_train, final_avg_auc_roc_test = run_log_reg_on_ttsplit(stratified_split, X, y, target,n_splits, pipe_logreg)
-
-    auctrain_list.append(final_avg_auc_roc_train)
-    auctest_list.append(final_avg_auc_roc_test)
-
-    mystop=1
-
-write_list_to_file(auctrain_list, f'{outputdir}/train_auc_roc_shuffle_list.csv')
-write_list_to_file(auctest_list, f'{outputdir}/test_auc_roc_shuffle_list.csv')
 
 
 mystop=1
